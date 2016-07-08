500dbdce828cd8785155402ed1485d1c91b16c91
kstanisz <staniszewski.kacper@gmail.com>
2016-07-09 00:53:18 +0200

    Updated pom.xml Created PointOfSaleServiceImp unit tests class.

---
+++ src/test/java/service/PointOfSaleServiceImplUnitTests.java
@@ -0,0 +1,35 @@
+package service;
+
+import device.BarcodeScanner;
+import device.Display;
+import device.Printer;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import repository.ProductRepository;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PointOfSaleServiceImplUnitTests {
+    @Mock
+    private BarcodeScanner barcodeScanner;
+    @Mock
+    private Display display;
+    @Mock
+    private Printer printer;
+    @Mock
+    private ProductRepository productRepository;
+    private ReceiptService receiptService = new ReceiptServiceImpl();
+    private PointOfSaleService pointOfSaleService;
+
+    @Before
+    public void setUpPointOfSaleService() {
+        pointOfSaleService = new PointOfSaleServiceImpl(barcodeScanner, display, printer, productRepository, receiptService);
+    }
+
+    @Test
+    public void testScanProduct(){
+
+    }
+}
