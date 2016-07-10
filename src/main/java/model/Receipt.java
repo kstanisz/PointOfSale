99241c8c18b7e996024d2fb2f453a6c2c8be5bc9
kstanisz <staniszewski.kacper@gmail.com>
2016-07-10 12:50:21 +0200

    Added hashCode for hashCode and equals contract

--- src/main/java/model/Receipt.java
+++ src/main/java/model/Receipt.java
@@ -3,6 +3,7 @@ package model;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Receipt {
     private BigDecimal totalPrice = BigDecimal.ZERO;
@@ -19,6 +20,11 @@ public class Receipt {
     public void addProductAndUpdateTotalPrice(Product product) {
         productList.add(product);
         totalPrice = totalPrice.add(product.getPrice());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.totalPrice, this.productList);
     }
 
     @Override
